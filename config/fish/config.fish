6cf3411ba8249b5c9eb57d1231b4438215a5d3f0
Ian Mikhailov <1557291+yanmhlv@users.noreply.github.com>
2024-07-14 15:56:29 -0300

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,8 @@ set -g fish_greeting
 /opt/homebrew/bin/brew shellenv | source
 starship init fish | source
 fzf --fish | source
+kubectl completion fish | source
+helm completion fish | source
 
 fish_add_path (go env GOPATH)/bin
 
@@ -23,6 +25,14 @@ abbr c 'clear'
 abbr k 'kubectl'
 abbr kgp 'kubectl get pods'
 abbr h 'helm'
+
+function kns -d "kubens"
+    kubectl get ns --no-headers -o wide | fzf --bind 'enter:become(kubectl config set-context --current --namespace={1})'
+end
+
+function kc -d "kubectx"
+    kubectl config get-contexts --no-headers -o name | fzf --bind 'enter:become(kubectl config use-context {-1})'
+end
 
 set fzf_history_time_format %d-%m-%y
 set fzf_git_log_format "%H %s"
