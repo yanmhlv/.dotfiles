ad9e2408555267a45cf21e3e83e7472659b299c6
Ian Mikhailov <1557291+yanmhlv@users.noreply.github.com>
2024-07-16 06:58:02 -0300

    Office laptop (#1)
    * [office-laptop] merge office laptop configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,27 +4,27 @@ end
 
 set -g fish_greeting
 
-/opt/homebrew/bin/brew shellenv | source
-starship init fish | source
-fzf --fish | source
-kubectl completion fish | source
-helm completion fish | source
+/opt/homebrew/bin/brew shellenv fish | source
+starship init fish                   | source
+fzf --fish                           | source
+kubectl completion fish              | source
+helm completion fish                 | source
 
 fish_add_path (go env GOPATH)/bin
 
 export FZF_DEFAULT_COMMAND='fd'
 export GIT_EXTERNAL_DIFF="difft"
 
-abbr l 'eza --grid'
-abbr la 'eza --grid --all'
-abbr ll 'eza -al --group-directories-first'
+abbr l    'eza --grid'
+abbr la   'eza --grid --all'
+abbr ll   'eza -al --group-directories-first'
 abbr tree 'eza --tree'
 
 abbr c 'clear'
 
-abbr k 'kubectl'
+abbr k   'kubectl'
 abbr kgp 'kubectl get pods'
-abbr h 'helm'
+abbr h   'helm'
 
 function kns -d "kubens"
     kubectl get ns --no-headers -o wide | fzf --bind 'enter:become(kubectl config set-context --current --namespace={1})'
