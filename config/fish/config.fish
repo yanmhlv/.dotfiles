505a217cbe01da7985baae659c33bced3bfee058
Ian Mikhailov <1557291+yanmhlv@users.noreply.github.com>
2024-12-25 12:25:30 +0300

    [main] upd fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -45,30 +45,39 @@ bind -M insert \cr _fzf_search_history
 
 fzf_configure_bindings --directory=\ct --processes=\ck --git_log=\cg
 
-# ozon helpers
-function warden-dev --argument service port
-  set response (curl -s "https://warden-dev.o3.ru/endpoints?service=$service:$port")
-  echo $response | jq -r -c '(sort_by(.ReleaseName).[] | {Role,ReleaseName,IsDefaultRoute,Address,InstanceName})' | fzf --reverse --multi --bind ctrl-a:select-all
-end
+#### ozon helpers
+# function warden-dev --argument service port
+#   set response (curl -s "https://warden-dev.o3.ru/endpoints?service=$service:$port")
+#   echo $response | jq -r -c '(sort_by(.ReleaseName).[] | {Role,ReleaseName,IsDefaultRoute,Address,InstanceName})' | fzf --reverse --multi --bind ctrl-a:select-all
+# end
 
-function warden-stg --argument service port
-  set response (curl -s "https://warden-stg.o3.ru/endpoints?service=$service:$port")
-  echo $response | jq -r -c '(sort_by(.ReleaseName).[] | {Role,ReleaseName,IsDefaultRoute,Address,InstanceName})' | fzf --reverse --multi --bind ctrl-a:select-all
-end
+# function warden-stg --argument service port
+#   set response (curl -s "https://warden-stg.o3.ru/endpoints?service=$service:$port")
+#   echo $response | jq -r -c '(sort_by(.ReleaseName).[] | {Role,ReleaseName,IsDefaultRoute,Address,InstanceName})' | fzf --reverse --multi --bind ctrl-a:select-all
+# end
 
-function warden-prod --argument service port
-  set response (curl -s "https://warden-prod.o3.ru/endpoints?service=$service:$port")
-  echo $response | jq -r -c '(sort_by(.ReleaseName).[] | {Role,ReleaseName,IsDefaultRoute,Address,InstanceName})' | fzf --reverse --multi --bind ctrl-a:select-all
-end
+# function warden-prod --argument service port
+#   set response (curl -s "https://warden-prod.o3.ru/endpoints?service=$service:$port")
+#   echo $response | jq -r -c '(sort_by(.ReleaseName).[] | {Role,ReleaseName,IsDefaultRoute,Address,InstanceName})' | fzf --reverse --multi --bind ctrl-a:select-all
+# end
 
 # function get-tokens --argument addr
 #     set response (curl -s -XGET "http://$addr/s2s-auth")
 #     echo $response | jq -c '[.tokens | .iat] | min'
 # end
 
+
+### kubernetes helpers
 function pod_log
     set pod (kubectl get pods -o name | fzf)
     kubectl logs -f --tail 5000 $pod | fzf --tail 5000 --tac --wrap --multi
 end
 
+function pod_connect
+    set pod (kubectl get pods -o name | fzf)
+    kubectl exec $pod -it -- sh
+end
+
+
+# fzf tail
 abbr ftail 'fzf --tail 5000 --tac --wrap --multi'
